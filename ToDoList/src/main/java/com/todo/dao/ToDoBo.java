aeb722df854d240f2f5dc212d60cbc43e1005a7a
Purushotham Palla <138353046+Purushotham-Palla@users.noreply.github.com>
2024-05-26 10:27:21 +0530

    Update ToDoBo.java

--- ToDoList/src/main/java/com/todo/dao/ToDoBo.java
+++ ToDoList/src/main/java/com/todo/dao/ToDoBo.java
@@ -9,10 +9,8 @@ public interface ToDoBo {
 	int save(ToDo td);
 	int update(ToDo td);
 	int delete(int todoId);
-	ToDo  getOne(int todoId);
 	ToDo  getOneTodo(int todoId);
 	ToDo  getOneProject(int projectId);
-	ToDo  get_One(int todoId,int projectId);
 	List<ToDo > getAll(int projectId);
 	
 
